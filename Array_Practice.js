425c52bae12b8de328f3da3021f2c4369db07d73
amrendra1998 <techupdate855@gmail.com>
2025-05-04 22:33:46 +0530

    File commited

---
+++ Array_Practice.js
@@ -0,0 +1,49 @@
+arr1 = [1,2,3,4,5];
+// console.log(arr1);
+
+// console.log(arr1[2]);
+
+let fruits = ["apple", "banana", "cherry", "date"];
+// console.log(fruits[0]); 
+
+// let person = new Array("John", "Doe", 30); --> New keyword 
+
+// console.log(arr1);
+
+// console.log(arr1.push(6))
+
+// console.log(arr1);
+// console.log(arr1.pop());
+// console.log(arr1);
+// console.log(arr1.length);
+// arr1.shift(); 
+// console.log(arr1);
+// console.log(arr1.unshift(8));
+// console.log(arr1);
+// console.log(arr1.includes(9)); 
+// console.log(arr1);
+// console.log(arr1.indexOf(3));
+
+// Slice and Splice 
+
+
+const Arr2 = ["shakti", "sita", "ram", "laxman"];
+// console.log(Arr2);
+const myn1 = Arr2.slice(1, 3); 
+// console.log(myn1);
+
+const myn2 = Arr2.splice(1, 3);
+// console.log(myn2); 
+
+//  For each () --> used for traversing the array
+
+const fruits1 = ["apple", "banana", "cherry", "date"];
+
+fruits1.forEach((fruits1 , index) => {
+    console.log(`fruit at index${index} :${fruits1}`);
+});
+
+
+
+
+
---
+++ Date_time.js
@@ -0,0 +1,9 @@
+let date = new Date("2023-10-01");
+// console.log(date); 
+console.log(date.toString());
+console.log(date.toLocaleString);
+
+// Array
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+
