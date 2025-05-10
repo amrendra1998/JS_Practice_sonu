063e5414512a33f83f23564f9d49ff7cc3f39bd1
amrendra1998 <techupdate855@gmail.com>
2025-05-10 23:22:59 +0530

    file commited

---
+++ control_flow.js
@@ -0,0 +1,57 @@
+// let num = 15
+// // if (num >= 2) {
+// //     console.log("nummber will print");
+    
+// // }
+// // else{
+// //     console.log(`no value will print`);
+    
+// // }
+// conditional Statement 
+let score = 85;
+if (score >= 90) {
+    console.log("print will A");
+    
+}
+else if (score >= 80) {
+    console.log("print Will B");
+    
+}
+else if (score >= 70) {
+    console.log("print Will C");
+    
+    
+}
+
+else{
+    console.log("No value Print");
+    
+}
+
+// Switch Statement 
+let day = 6;
+switch (day) {
+    case 1: console.log("it will print Monday");
+    break;
+    case 2: console.log("it will print Tuesday");
+    break;
+    case 3: console.log("it will print Wednesday");
+    break;
+    case 4: console.log("it will print Thrusday");
+    break;
+    case 5: console.log("it will print Friday");
+    break;
+    case 6: console.log("it will print Saturday");
+    break;
+    case 7: console.log("it will print Sunday");
+    break;
+
+    default:
+        console.log("weekend");
+        
+        break;
+}
+//  Loops control Flow statement 
+
+
+
